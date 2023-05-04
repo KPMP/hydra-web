529ddd5c0c0980b57dec86f7c104ff7c474ce28a
Dert1129 <nathancreger1111@gmail.com>
2023-05-04 13:38:52 -0400

    add react-tooltip css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap-css-only/css/bootstrap.min.css';
 import './index.css';
+import 'react-tooltip/dist/react-tooltip.css'
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
