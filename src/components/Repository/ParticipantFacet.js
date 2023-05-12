9aae6bff4e65e881cb3811393b8660a29bdf6c28
Zach Wright <zwright@umich.edu>
2023-05-12 11:18:42 -0400

    KPMP-4277: try accordion

--- src/components/Repository/ParticipantFacet.js
+++ src/components/Repository/ParticipantFacet.js
@@ -1,23 +1,50 @@
 import React, {Component} from 'react';
-import {  Col, Container, Row } from "reactstrap";
+import {  Col, Container, Row, UncontrolledAccordion } from "reactstrap";
 import { Facet } from "@elastic/react-search-ui";
 import { MultiCheckboxFacet } from "@elastic/react-search-ui-views";
 
 class ParticipantFacet extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {};
+
+    }
+
     render() {
         return (
             <Container className="mt-3 rounded border p-3 shadow-sm spatial-filter-panel container-max">
+                <UncontrolledAccordion
+                    defaultOpen={[
+                        '1',
+                        '2'
+                    ]}
+                    stayOpen
+                >
+                    <AccordionItem>
+                        <AccordionHeader targetId="1">
+                            Accordion Item 1
+                        </AccordionHeader>
+                        <AccordionBody accordionId="1">
                 <Row className='mb-2'>
                     <Col>
                         <Facet field='sex' label='Sex' filterType='any' view={MultiCheckboxFacet}/>
                     </Col>
                 </Row>
+                        </AccordionBody>
+                    </AccordionItem>
+                    <AccordionItem>
+                        <AccordionHeader targetId="2">
+                            Accordion Item 1
+                        </AccordionHeader>
+                        <AccordionBody accordionId="2">
                 <Row className='mb-2'>
                     <Col>
                         <Facet field='age_binned' label='Age' filterType='any' view={MultiCheckboxFacet}/>
                     </Col>
                 </Row>
+                        </AccordionBody>
+                    </AccordionItem>
                 <Row className='mb-2'>
                     <Col>
                         <Facet field='tissue_type' label='Tissue Type' filterType='any' view={MultiCheckboxFacet}/>
