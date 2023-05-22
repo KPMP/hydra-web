a5320093730d34ca7c0a07050ebe736cf2fa5391
HaneenT <haneen@umich.edu>
2023-05-22 16:09:05 -0400

    KPMP-4463: small edits to ToolbarButton

--- src/components/Repository/FileList.js
+++ src/components/Repository/FileList.js
@@ -497,8 +497,7 @@ class FileList extends Component {
                                             cards={this.state.cards}
                                             setCards={this.setCards}
                                             setDefaultCards={this.setDefaultCards}
-                                            // sortableToolbarColumns={this.state.cards}
-                                            defaultOrder={this.getColumns()} />
+                                            defaultOrder={this.getColumns().map(item => item.name)} />
                                         <PaginationState
                                             currentPage={currentPage}
                                             setTableSettings={this.props.props.setTableSettings}
