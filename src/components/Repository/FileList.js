4efaef0f90ecf5136fbf005811450c117f63f1be
HaneenT <haneen@umich.edu>
2023-07-31 15:38:43 -0400

    remove spinner

--- src/components/Repository/FileList.js
+++ src/components/Repository/FileList.js
@@ -384,14 +384,6 @@ class FileList extends Component {
 
         return (
             <Container id='outer-wrapper' className="multi-container-container container-xxl mh-100">
-                
-                { this.state.reportIsLoading === true &&
-                    <div className='spinner-container'>
-                        <Spinner className='report-spinner'>
-                                Loading
-                        </Spinner>
-                    </div>
-                }
                 <Row>
                     <Col xl={3} className={`filter-panel-wrapper ${this.props.filterTabActive ? '': 'hidden'}`}>
                         <div className={`filter-panel-wrapper ${this.props.filterTabActive ? '': 'hidden'}`}>
