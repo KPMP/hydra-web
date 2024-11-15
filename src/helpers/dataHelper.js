a3f7350d19c2d3366eb236d6baf83c61259f003d
dert1129 <nathancreger1111@gmail.com>
2024-11-15 09:49:21 -0500

    change raas blockade

--- src/helpers/dataHelper.js
+++ src/helpers/dataHelper.js
@@ -66,7 +66,7 @@ export const mapClinicalKeysToPresentationStyle = (data) => {
     result['Hypertension Duration (years)'] = "";
     result['Hypertension History'] = "";
     result['KDIGO Stage'] = "";
-    result['On RAAS Blockade'] = "";
+    result['RAAS Blockade'] = "";
     result['Proteinuria (mg)'] = "";
     result['Race'] = "";
     result['Age (Years)'] = "";
@@ -103,7 +103,7 @@ export const mapClinicalKeysToPresentationStyle = (data) => {
         result['KDIGO Stage'] = data.kdigoStage ? data.kdigoStage : "";
     }
     if (data.onRaasBlockade) {
-        result['On RAAS Blockade'] = data.onRaasBlockade ? data.onRaasBlockade : "";
+        result['RAAS Blockade'] = data.onRaasBlockade ? data.onRaasBlockade : "";
     }
     if (data.proteinuria) {
         result['Proteinuria (mg)'] = data.proteinuria ? data.proteinuria : "";
